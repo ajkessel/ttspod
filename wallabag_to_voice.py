ba443997e28a038f8296c302649cff76edf40bfd
Adam Kessel <adam@rosi-kessel.org>
2024-09-16 17:54:49 -0400

    More initial fixes

--- wallabag_to_voice.py
+++ wallabag_to_voice.py
@@ -37,10 +37,16 @@ try:
     grad_available = True
 except ImportError:
     grad_available = False
+try:
+    from TTS.api import TTS
+    tts_available = True
+except ImportError:
+    tts_available = False
 
 from dotenv import load_dotenv
 from pathlib import Path
 from pydub import AudioSegment
+from io import BytesIO
 import html2text
 import json
 import os
@@ -82,6 +88,7 @@ class WallPod(object):
         global eleven_available
         global tortoise_available
         global grad_available
+        global tts_available
         load_dotenv()
         self.debug = 'wallpod_debug' in os.environ
         self.wallabagUrl = os.environ['url']
@@ -103,6 +110,10 @@ class WallPod(object):
         self.engine = os.environ['engine'].lower() if 'engine' in os.environ else ""
         self.grad_tts = os.environ['grad_tts'] if 'grad_tts' in os.environ else ""
         self.hifigan = os.environ['hifigan'] if 'hifigan' in os.environ else ""
+        self.pe_scale = int(os.environ['pe_scale']) if 'pe_scale' in os.environ else None
+        self.n_spks = int(os.environ['n_spks']) if 'pe_scale' in os.environ else None
+        self.tts_model = os.environ['wallpod_tts_model'] if 'wallpod_tts_model' in os.environ else None
+        self.tts_vocoder_model = os.environ['wallpod_tts_vocoder_model'] if 'wallpod_tts_vocoder_model' in os.environ else None
         if self.engine in 'openai' and self.openai_api_key and openai_available:
             self.engine = 'openai'
             self.openaiclient = OpenAI(api_key = self.openai_api_key)
@@ -117,7 +128,13 @@ class WallPod(object):
             self.voice_samples, self.conditioning_latents = load_voices(['daniel'])
         elif self.engine in 'grad' and grad_available and self.grad_tts and self.hifigan:
             self.engine = 'grad'
-            self.tts = Gradify(self.grad_tts, self.hifigan)
+            self.tts = Gradify(self.grad_tts, self.hifigan,self.pe_scale,self.n_spks)
+        elif self.engine in 'tts' and self.tts_model and tts_available:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            if self.tts_vocoder_model:
+                self.tts = TTS(model_name = self.tts_model, vocoder_path = self.tts_vocoder_model).to(device)
+            else:
+                self.tts = TTS(model_name = self.tts_model).to(device)
         else:
             raise Exception("no TTS engine/API key found")
         return
@@ -178,7 +195,7 @@ class WallPod(object):
                 )
 
     def speechify(self, title, text):
-        out_file = slugify(title) + ".mp3"
+        out_file = self.final_path + slugify(title) + ".mp3"
         paragraphs = re.split(r'(\n|\r){2,}',text)
         segments = []
         add_to_next = ""
@@ -200,47 +217,56 @@ class WallPod(object):
                     segments.append(this_segment)
         else:
             segments = split_and_recombine_text(text)
-        item = 1
         combined = AudioSegment.empty()
-        for segment in segments:
-            if self.debug: print(f'processing text:\n{segment}\n--------------------')
-            if self.engine == "eleven":
-                audio = self.elevenclient.generate(
-                    text=segment,
-                    voice="Daniel",
-                    model="eleven_monolingual_v1"
-                    )
-                save(audio, f'{self.temp_path}{item}.mp3')
-            elif self.engine == "openai":
-                response = self.openaiclient.audio.speech.create(
-                    model="tts-1",
-                    voice="alloy",
-                    input=segment
-                    )
-                response.stream_to_file(f'{self.temp_path}{item}.mp3')
-            elif self.engine == 'tortoise':
-                gen, dbg_state = self.tts.tts_with_preset(text=segment, k=1, voice_samples=self.voice_samples, conditioning_latents=self.conditioning_latents,
-                                  preset='fast', return_deterministic_state=True, cvvp_amount=0)
-                torchaudio.save(f'{self.temp_path}{item}.mp3', gen.squeeze(0).cpu(), 24000)
-            elif self.engine == 'grad':
-                self.tts.transcribe(segment,f'{self.temp_path}{item}.mp3')
+        for (item,segment) in enumerate(segments):
+            segment_audio = f'{self.temp_path}{item}.mp3'
+            if self.debug: print(f'processing text #{item} length {len(segment)}:\n{segment}\n--------------------')
+            try:
+                if self.engine == "eleven":
+                    audio = self.elevenclient.generate(
+                        text=segment,
+                        voice="Daniel",
+                        model="eleven_monolingual_v1"
+                        )
+                    save(audio, segment_audio)
+                elif self.engine == "openai":
+                    response = self.openaiclient.audio.speech.create(
+                        model="tts-1",
+                        voice="alloy",
+                        input=segment
+                        )
+                    response.stream_to_file(segment_audio)
+                elif self.engine == 'tortoise':
+                    gen, dbg_state = self.tts.tts_with_preset(text=segment, k=1, voice_samples=self.voice_samples, conditioning_latents=self.conditioning_latents,
+                                    preset='fast', return_deterministic_state=True, cvvp_amount=0)
+                    torchaudio.save(segment_audio, gen.squeeze(0).cpu(), 24000)
+                elif self.engine == 'grad':
+                    self.tts.transcribe(segment,segment_audio,200)
+                elif self.engine == 'tts':
+                    self.tts.tts_to_file(text=segment,file_path="output.wav",split_sentences=False)
+                    AudioSegment.from_file("output.wav").export(segment_audio,format='mp3')
+                    # TODO use BytesIO and perform in RAM
+                else:
+                    raise Exception("no TTS engine found")
+            except:
+                print("TTS failed")
             else:
-                raise Exception("no TTS engine found")
-            combined += AudioSegment.from_mp3(f'{self.temp_path}{item}.mp3')
-        combined.export(f'{self.final_path}{out_file}', format="mp3")
-        os.chmod(f'{self.final_path}{out_file}', 0o644)
+                combined += AudioSegment.from_mp3(segment_audio)
+        combined.export(out_file, format="mp3")
+        os.chmod(out_file, 0o644)
         return (out_file)
 
     def process(self):
         entries = self.getEntries()
         for entry in entries:
             (title, content, url) = entry
-            if url in self.cached:
+            if url in self.cache:
                 if self.debug: print(f'{title} is already in the feed, skipping')
                 continue
             if self.debug: print(f'processing {title}')
-            filename = self.speechify(title, content)
-            size = os.path.getsize(f'{self.final_path}{filename}')
+            fullpath = self.speechify(title, content)
+            filename = ntpath.split(fullpath)[1]
+            size = os.path.getsize(f'{fullpath}')
             self.p.episodes.append(
                 pod2gen.Episode(
                     title = title,
