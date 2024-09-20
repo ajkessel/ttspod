c6107a849190544078514e7954daf80284a1597e
Adam Kessel <adam@rosi-kessel.org>
2024-09-20 17:16:48 -0400

    added quickstart script

--- quickstart.sh
+++ quickstart.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+echo creating local python venv under current directory
 python3 -m venv .venv
 source .venv/bin/activate
+echo installing requirements
 pip install -r requirements.txt
 optional=$(cat 'optional-requirements.txt')
+echo optional requirements
 for line in $optional
 do
   echo -n "Install optional requirement ${line}? (y/n) "
@@ -12,3 +15,5 @@ do
     pip install "$line"
   fi
 done
+echo Just edit .venv to configure your local settings and you will be good to go.
+cp -i dotenv .env
