203bc49803035418929d959bd6c4509c421fc450
Adam Kessel <adam@rosi-kessel.org>
2024-09-20 17:15:34 -0400

    added quickstart script

--- quickstart.sh
+++ quickstart.sh
@@ -3,11 +3,11 @@ python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
 optional=$(cat 'optional-requirements.txt')
-for line in optional
+for line in $optional
 do
   echo -n "Install optional requirement ${line}? (y/n) "
   read answer
-  if [ "$answer" == "y" ]]
+  if [ "$answer" == "y" ]
   then
     pip install "$line"
   fi
