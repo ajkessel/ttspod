0d114066338e2d2d43dc0baa57b238555af87779
Adam Kessel <adam@rosi-kessel.org>
2024-09-21 13:15:50 -0400

    util module

---
+++ util.py
@@ -0,0 +1,26 @@
+from pypandoc import convert_text
+from html import unescape
+import re
+
+def cleanHTML(rawhtml):
+    text = convert_text(
+            rawhtml,
+            'plain',
+            format='html',
+            extra_args=['--wrap=none', '--strip-comments']
+            )
+    text = self.cleanText(text)
+    return text
+
+def cleanText(text):
+    try:
+        text = unescape(text)
+        text = text.replace(u'\u201c', '"').replace(u'\u201d', '"').replace(u'\u2018',"'").replace(u'\u2019',"'").replace(u'\u00a0',' ')
+        text = re.sub(r'[^A-Za-z0-9 \n\/\(\)_.,!"\']',' ',text)
+        text = re.sub(r'^ *$','\n',text,flags = re.MULTILINE)
+        text = re.sub(r'\n\n+','\n\n',text)
+        text = re.sub(r' +',' ',text)
+        text = text.strip()
+    except:
+        pass
+    return text
