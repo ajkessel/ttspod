fc17683078b25d47a98555bac683982170bcee48
Adam Kessel <adam@rosi-kessel.org>
2024-09-24 15:37:07 -0400

    minor fixes

---
+++ quickstart.ps1
@@ -0,0 +1,5 @@
+# Windows install is not ready yet
+# But you'll need to create a conda or python environment
+# Install pip install requirements.txt and optional-requirements.txt
+# Also install pip install python-magic-bin
+# You may need to run as administator to download speech models for Whisper
