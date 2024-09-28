fb6f6bcaa3e2b819fba10294abebddff4af3f1b1
Adam Kessel <adam@rosi-kessel.org>
2024-09-27 20:46:48 -0400

    bug fixes

--- quickstart.sh
+++ quickstart.sh
@@ -22,130 +22,131 @@ venv() {
         sudo apt install python3.11-venv
       fi
     fi
-    if [ ! -e .venv/bin/activate ]
-    then
-      echo Virtual environment creation failed. Exiting.
-      exit 1
-    fi
-    source .venv/bin/activate
-    echo installing requirements
-    pip3 install -r requirements.txt
-    optional=$(cat 'optional-requirements.txt')
-    echo 'optional requirements - you should install at least one TTS engine (Whisper, Coqui "TTS", OpenAI, or Eleven)'
-    echo 'also install truststore if you need to trust locally-installed certificates (e.g. due to a firewall/VPN)'
-    for line in $optional
-    do
-      if yesno "Install optional requirement ${line}?"
-      then
-        pip3 install "$line"
-      fi
-    done
-  }
-  title() {
-    len="${#1}"
-    pad=$((30-((len/2))))
-    padding=$(printf -- '-%.0s' `seq 1 $pad`)
-    echo "${padding} ${1} ${padding}"
-  }
-  footer() {
-    printf -- '--------------------------------------------------------------\n\n'
-  }
-
-  [ $(uname) == "Darwin" ] && MAC=1
-  command -v brew &> /dev/null && BREW=1
-  [ $EDITOR ] || command -v nano &> /dev/null && EDITOR=nano || command -v vim &> /dev/null && EDITOR=vim || command -v vi &> /dev/null && EDITOR=vi 
-
-  title TTSPod Installer
-  echo This will set things up under your current directory `pwd`
-  if ! yesno 'Proceed?'
+  fi
+  if [ ! -e .venv/bin/activate ]
   then
-    echo OK, exiting.
+    echo Virtual environment creation failed. Exiting.
+    exit 1
+  fi
+  source .venv/bin/activate
+  echo installing requirements
+  pip3 install -r requirements.txt
+  optional=$(cat 'optional-requirements.txt')
+  echo 'optional requirements - you should install at least one TTS engine (Whisper, Coqui "TTS", OpenAI, or Eleven)'
+  echo 'also install truststore if you need to trust locally-installed certificates (e.g. due to a firewall/VPN)'
+  for line in $optional
+  do
+    if yesno "Install optional requirement ${line}?"
+    then
+      pip3 install "$line"
+    fi
+  done
+}
+title() {
+  len="${#1}"
+  pad=$((30-((len/2))))
+  padding=$(printf -- '-%.0s' `seq 1 $pad`)
+  echo "${padding} ${1} ${padding}"
+}
+footer() {
+  printf -- '--------------------------------------------------------------\n\n'
+}
+
+[ $(uname) == "Darwin" ] && MAC=1
+command -v brew &> /dev/null && BREW=1
+[ $EDITOR ] || command -v nano &> /dev/null && EDITOR=nano || command -v vim &> /dev/null && EDITOR=vim || command -v vi &> /dev/null && EDITOR=vi 
+
+title TTSPod Installer
+echo This will set things up under your current directory `pwd`
+if ! yesno 'Proceed?'
+then
+  echo OK, exiting.
+  exit 0
+fi
+
+if ! command -v pip3 > /dev/null 2>&1
+then
+  echo pip3 not found, exiting.
+  exit 1
+fi
+footer
+
+title Python
+pyexe="python3.11"
+if ! command -v "${pyexe}" &> /dev/null
+then
+  echo 'This is only tested with python3.11, which seems to be missing from your system.'
+  if [ $MAC ] && [ $BREW ]
+  then
+    if yesno 'Do you want to install with homebrew?'
+    then
+      brew install python@3.11
+    fi
+  elif yeson 'Do you want to proceed anyway?'
+  then
+    pyexe=python3
+  else
     exit 0
   fi
+fi
 
-  if ! command -v pip3 > /dev/null 2>&1
+if ! command -v "${pyexe}" &> /dev/null
+then
+  echo "${pyexe} not found, exiting."
+  exit 1
+fi
+
+echo "${pyexe} located."
+footer
+
+title 'venv'
+if [ -d "./.venv" ]
+then
+  if yesno 'A .venv folder already exists under `pwd`. Do you want to move it out of the way and generate fresh?'
   then
-    echo pip3 not found, exiting.
-    exit 1
+    timestamp=$(date +%s)
+    mv ".venv" ".venv-${timestamp}"
+    echo ".venv moved to .venv-${timestamp}"
+  elif ! yesno 'Do you want to install into the existing .venv?'
+  then
+    skipvenv=1
   fi
-  footer
+fi
 
-  title Python
-  pyexe="python3.11"
-  if ! command -v "${pyexe}" &> /dev/null
+[ -z "${skipvenv}" ] && venv
+footer
+
+if [ $MAC ]
+then
+  title 'mac install'
+  echo 'MacOS environment detected.'
+  if [ $BREW ]
   then
-    echo 'This is only tested with python3.11, which seems to be missing from your system.'
-    if [ $MAC ] && [ $BREW ]
+    if ! brew list libmagic > /dev/null 2>&1
     then
-      if yesno 'Do you want to install with homebrew?'
+      if yesno 'ttspod requires libmagic. install with brew?'
       then
-        brew install python@3.11
-      fi
-    elif yeson 'Do you want to proceed anyway?'
-    then
-      pyexe=python3
-    else
-      exit 0
-    fi
-  fi
-
-  if ! command -v "${pyexe}" &> /dev/null
-  then
-    echo "${pyexe} not found, exiting."
-    exit 1
-  fi
-
-  echo "${pyexe} located."
-  footer
-
-  title 'venv'
-  if [ -d "./.venv" ]
-  then
-    if yesno 'A .venv folder already exists under `pwd`. Do you want to move it out of the way and generate fresh?'
-    then
-      timestamp=$(date +%s)
-      mv ".venv" ".venv-${timestamp}"
-      echo ".venv moved to .venv-${timestamp}"
-    elif ! yesno 'Do you want to install into the existing .venv?'
-    then
-      skipvenv=1
-    fi
-  fi
-
-  [ -z "${skipvenv}" ] && venv
-  footer
-
-  if [ $MAC ]
-  then
-    title 'mac install'
-    echo 'MacOS environment detected.'
-    if [ $BREW ]
-    then
-      if ! brew list libmagic > /dev/null 2>&1
-      then
-        if yesno 'ttspod requires libmagic. install with brew?'
-        then
-          brew install libmagic
-        fi
-      else
-        echo libmagic already installed
+        brew install libmagic
       fi
     else
-      echo 'tts requires libmagic, but I did not find a brew installation.'
+      echo libmagic already installed
     fi
-    footer
-  fi
-
-  title 'customize'
-  cp -i dotenv .env
-  echo Just edit .env to configure your local settings and you will be good to go.
-  if yesno "Do you want to edit .env now?"
-  then
-    if [ -z "${EDITOR}" ]
-    then
-      echo no editor found
-    fi
-    "${EDITOR}" .env
+  else
+    echo 'tts requires libmagic, but I did not find a brew installation.'
   fi
   footer
-  echo get help with ./ttspod -h
+fi
+
+title 'customize'
+cp -i dotenv .env
+echo Just edit .env to configure your local settings and you will be good to go.
+if yesno "Do you want to edit .env now?"
+then
+  if [ -z "${EDITOR}" ]
+  then
+    echo no editor found
+  fi
+  "${EDITOR}" .env
+fi
+footer
+echo get help with ./ttspod -h
