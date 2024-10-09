e915ef84a4e4a5f89835a7b4d10c25c953e2fd40
Adam Kessel <adam@rosi-kessel.org>
2024-10-08 20:41:41 -0400

    minor fixes

--- src/ttspod/util.py
+++ src/ttspod/util.py
@@ -243,8 +243,8 @@ except ImportError:
             )
             results += str(result.stdout) + str(result.stderr)
         new_version = re.search(r'Downloading ttspod-([0-9\.]*)', results)
-        if new_version:
-            print(f'Upgraded to {new_version[0]}.')
+        if new_version: # TODO not working
+            print(f'Upgraded to {new_version[1]}.')
         if "error" in results.lower():
             print(
                 f'results of upgrade below, error may have occurred:\n{results}\n')
