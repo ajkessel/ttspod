0d480a55f4e9e28bc646643ce30d1a5eba111365
Adam Kessel <adam@rosi-kessel.org>
2024-10-16 15:44:53 -0400

    minor fixes

--- src/ttspod/speech/__init__.py
+++ src/ttspod/speech/__init__.py
@@ -4,3 +4,4 @@ import sys
 from pathlib import Path
 # allow components to be run separately
 sys.path.append(Path(os.path.realpath(__file__)).parent)
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
