5e2869b8488a44909985b4a186b3c7bafd4d34b0
Adam Kessel <adam@rosi-kessel.org>
2024-11-06 11:01:18 -0500

    coqui fixes

--- src/ttspod/speech/coqui.py
+++ src/ttspod/speech/coqui.py
@@ -84,6 +84,5 @@ class Coqui:
 if __name__ == "__main__":
     coqui = Coqui()
     print("This is the TTSPod Coqui TTS module.")
-    # pprint(vars(coqui))
-    # pprint(dir(coqui))
-    coqui.convert("this is a test of the coqui model.","output.wav")
+    pprint(vars(coqui))
+    pprint(dir(coqui))
--- src/ttspod/version.py
+++ src/ttspod/version.py
@@ -1,2 +1,2 @@
 """current version"""
-__version__ = '0.1.354'
+__version__ = '0.1.355'
