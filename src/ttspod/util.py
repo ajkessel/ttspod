bc86f62db7e60ad10935cc448be5b875e75ed745
Adam Kessel <adam@rosi-kessel.org>
2024-11-08 18:30:25 -0500

    dependencies

--- src/ttspod/util.py
+++ src/ttspod/util.py
@@ -410,7 +410,7 @@ def upgrade(force=False, debug=False) -> bool:
             [
                 executable, "-m", "pip", "install",
                 "git+https://github.com/SWivid/F5-TTS.git@3fcdbc70b4a9d4299e1ecd0b5a1c35209f23fd69",
-                "--upgrade", "--force-reinstall", "--no-deps", "--upgrade-strategy", "eager"
+                "--upgrade", "--upgrade-strategy", "eager"
             ],
             # 3fcdbc70b4a9d4299e1ecd0b5a1c35209f23fd69 = v0.1.0
             # force-reinstall seems necessary to cause an upgrade from github
--- src/ttspod/version.py
+++ src/ttspod/version.py
@@ -1,2 +1,2 @@
 """current version"""
-__version__ = '0.1.425'
+__version__ = '0.1.426'
