57f126db3ceb1c2872fac30dbf639f38b9bf7566
Adam Kessel <adam@rosi-kessel.org>
2024-11-09 10:42:09 -0500

    restore mps workaround in xtts

--- src/ttspod/speech/xtts.py
+++ src/ttspod/speech/xtts.py
@@ -38,7 +38,7 @@ if torch.cuda.is_available():
 elif torch.backends.mps.is_available() and processor() != 'i386':
     DEVICE = 'mps'
     env["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
-    # pytorch_utils.isin_mps_friendly = patched_isin_mps_friendly
+    pytorch_utils.isin_mps_friendly = patched_isin_mps_friendly
 
 # cspell: disable
 if "cuda" in DEVICE and torch.cuda.get_device_name().endswith("[ZLUDA]"):
--- src/ttspod/version.py
+++ src/ttspod/version.py
@@ -1,2 +1,2 @@
 """current version"""
-__version__ = '0.1.428'
+__version__ = '0.1.429'
